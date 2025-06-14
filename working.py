7cd34d23bffd8cd2b3f35089d4b5049e10c59444
Rhythmic-Ocean <khanalsamir991@gmail.com>
2025-06-13 19:01:21 -0500

    fouth one

--- working.py
+++ working.py
@@ -40,25 +40,63 @@ def add_new_user():
 def log_og_pp():
     print("\nTHIS WILL TAKE A WHILE, PLEASE DO NOT TERMINATE THE PROCESS!\n")
     for i in range(8):
+        updates = []
         sheet = workbook.get_worksheet(i)
+        sheet_name = sheet.title
         print(f"\n Working on {sheet.title} sheet.\n")
         col_a_values = sheet.col_values(1)
         for j in range(3, len(col_a_values) + 1):
             player = col_a_values[j-1]
-            og_pp = get_pp(player)
+            og_pp = round(get_pp(player))
             if og_pp:
-                sheet.update(f"G{j}", [[og_pp]])
-                print(f"{player}'s og pp succssfully updated\n")
+                updates.append({
+                    "range" : f"{sheet_name}!G{j}",
+                    "values" : [[og_pp]]
+                })
+                print(f"{player}'s data queued for update\n")
             else:
                 print(f"error for {player}\n")
+        sheet.spreadsheet.values_batch_update({
+            "valueInputOption" : "USER_ENTERED",
+            "data" : updates
+        })
+        print(f"{sheet_name} updated succssfully")
 
+def log_pp_change():
+    print("\nTHIS WILL TAKE A WHILE, PLEASE DO NOT TERMINATE THE PROCESS!\n")
+    for i in range(8):
+        updates = []
+        sheet = workbook.get_worksheet(i)
+        sheet_name = sheet.title
+        print(f"\nWorking on {sheet_name} sheet. \n")
+        col_a_values = sheet.col_values(1)
+        for j in range(3, len(col_a_values) + 1):
+            player = col_a_values[j-1]
+            pp = round(get_pp(player))
+            if pp:
+                updates.append({
+                    "range" : f"{sheet_name}!H{j}",
+                    "values" : [[pp]]
+                })
+                updates.append({
+                    "range" : f"{sheet_name}!I{j}",
+                    "values" : [[f"=H{j}-G{j}"]]
+                })
+                print(f"{player}'s data queued for update\n")
+            else:
+                print(f"error for {player}\n")
+        sheet.spreadsheet.values_batch_update({
+            "valueInputOption": "USER_ENTERED",
+            "data": updates
+        })
+        print(f"{sheet_name} updated succssfully")
 
 def main():
     running = True
     while running:
         ans1 = input("Click 1 if you would like to add a new User\n" 
                         "Click 2 if you would like to log OG pp\n" \
-                        "Click 3 if you would want to log the pp change\n"
+                        "Click 3 if you would want to log current pp and pp change\n"
                         "Click 4 to safely exit: ")
         try:
             choice = int(ans1)
@@ -71,7 +109,8 @@ def main():
             break
         elif choice == 2:
             log_og_pp()
-        else:
+        elif choice == 3:
+            log_pp_change()
             continue
 
 if __name__ == "__main__":
